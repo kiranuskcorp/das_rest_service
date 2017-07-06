928415da1819cd6d09118c9254c95f3767073b5c
Anurag Gangone <anurag.uskcorp@gmail.com>
2017-07-06 16:56:51 +0530

    userpreparedstatement

--- das_rest_service/src/main/java/in/uskcorp/tool/das/dao/setter/UserPreparedStatementSetter.java
+++ das_rest_service/src/main/java/in/uskcorp/tool/das/dao/setter/UserPreparedStatementSetter.java
@@ -22,18 +22,17 @@ public class UserPreparedStatementSetter implements PreparedStatementSetter {
 	public void setValues(PreparedStatement arg0) throws SQLException {
 		arg0.setString(1, user.getName());
 		arg0.setString(2, user.getEmail());
-		arg0.setString(3, user.getPassword());
-		arg0.setString(4, user.getConfirmPassword());
-		arg0.setString(5, user.getPhone());
-		arg0.setString(6, user.getAlternatePhone());
-		arg0.setDate(7, ResultSetUtil.converttoSQLDate(user.getDob()));
-		arg0.setString(8, user.getGender());
-		arg0.setString(9, user.getAddress());
-		arg0.setString(10, user.getRole());
-		arg0.setDate(11, ResultSetUtil.converttoSQLDate(new Date()));
-		arg0.setString(12, user.getDescription());
+		arg0.setString(3, ResultSetUtil.getGeneratedPassword());
+		arg0.setString(4, user.getPhone());
+		arg0.setString(5, user.getAlternatePhone());
+		arg0.setDate(6, ResultSetUtil.converttoSQLDate(user.getDob()));
+		arg0.setString(7, user.getGender());
+		arg0.setString(8, user.getAddress());
+		arg0.setString(9, user.getRole());
+		arg0.setDate(10, ResultSetUtil.converttoSQLDate(new Date()));
+		arg0.setString(11, user.getDescription());
 		if (!isInsert) {
-			arg0.setInt(13, user.getId());
+			arg0.setInt(12, user.getId());
 		}
 	}
 }
